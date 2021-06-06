884c4cbe18b7bb6a312b7812f52aee540c94b27f
Yonatan Barzilay <shtrok10@cs.colman.ac.il>
2021-06-06 23:04:22 +0300

    Added Ingredeint Model

--- Models/Ingredient.cs
+++ Models/Ingredient.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace WebApp2021.Models
 {
-    class Ingredient
+    public class Ingredient
     {
+        public Ingredient()
+        {
+            Recipes = new List<RecipeIngredient>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int Carbs { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int Protein { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int Fat { get; set; }
+
+        [DisplayName("Calories per 100 gram")]
+        public int CaloriesPer100Gram
+        {
+            get { return Carbs * 4 + Protein * 4 + Fat * 9; }
+        }
+
+        [DisplayName("Kosher type")]
+        public KosherType KosherType { get; set; }
+
+        [JsonIgnore]
+        public virtual List<RecipeIngredient> Recipes { get; set; }
     }
 }
--- Models/RecipeIngredient.cs
+++ Models/RecipeIngredient.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WebApp2021.Models
+﻿namespace WebApp2021.Models
 {
-    class RecipeIngredient
+    public class RecipeIngredient
     {
+        public int RecipeId { get; set; }
+        public virtual Recipe Recipe { get; set; }
+
+        public int IngredientId { get; set; }
+        public virtual Ingredient Ingredient { get; set; }
     }
 }
